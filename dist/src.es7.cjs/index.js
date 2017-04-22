1d4183e941b65c56dee5e719c202b75d5ff44c3b
Offirmo <offirmo.net@gmail.com>
2017-04-22 11:52:09 +1000

    TLTWGCMOMPP :scream_cat:

--- dist/src.es7.cjs/index.js
+++ dist/src.es7.cjs/index.js
@@ -5,19 +5,18 @@ const express = require("express");
 console.log('Hello world from express server!');
 const PORT = process.env.PORT || 5000;
 const app = express();
-// trust first proxy
-app.set('trust proxy', 1);
+// https://expressjs.com/en/4x/api.html#app.settings.table
+app.enable('trust proxy');
+app.disable('x-powered-by');
 // https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public', {
-    index: false,
-    fallthrough: true
-}));
-// respond with "hello world" when a GET request is made to the homepage
+// REM: respond with index.html when a GET request is made to the homepage
+app.use(express.static('public'));
+// respond with "hello world" when a GET request is made to this path
 app.get('/hello', function (req, res) {
     res.send('hello world!');
 });
 const server = http_1.createServer(app);
-server.listen(PORT, err => {
+server.listen(PORT, (err) => {
     if (err)
         return console.error('something bad happened', err);
     console.info(`server is listening on ${PORT}`);
--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,26 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 
-// trust first proxy
-app.set('trust proxy', 1)
+// https://expressjs.com/en/4x/api.html#app.settings.table
+app.enable('trust proxy')
+app.disable('x-powered-by')
+
 
 // https://expressjs.com/en/starter/static-files.html
-app.use(express.static('public', {
-	index: false, // prevent directory indexing
-	fallthrough: true
-}))
+// REM: respond with index.html when a GET request is made to the homepage
+app.use(express.static('public'))
 
-// respond with "hello world" when a GET request is made to the homepage
+// respond with "hello world" when a GET request is made to this path
 app.get('/hello', function (req, res) {
 	res.send('hello world!')
 })
 
 const server = createServer(app)
 
-server.listen(PORT, err => {
+server.listen(PORT, (err: Error) => {
 	if (err)
 		return console.error('something bad happened', err)
 
 	console.info(`server is listening on ${PORT}`)
 })
+
